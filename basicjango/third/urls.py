a64e239339b103cad63b2648f68a70b66ccba6ea
khb <khb1928@kotech.co.kr>
2022-01-17 17:11:11 +0900

    CRUD

--- basicjango/third/urls.py
+++ basicjango/third/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('list/',views.list , name='list'),
+    path('create/', views.create , name='restaurants-create'),
+    path('update/', views.update , name='restaurants-update'),
+    path('detail/', views.detail , name='restaurants-detail'),
+    path('delete/',views.delete , name='restaurants-delete' )
 ]
